b2ac9cd3060773683eca5bf9b0c5e4be40c93edf
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-03-18 16:34:46 -0300

    Classe passageiro implementada e mapeada

--- src/main/java/br/com/fiap/springpfentregas/entity/Endereco.java
+++ src/main/java/br/com/fiap/springpfentregas/entity/Endereco.java
@@ -1,4 +1,32 @@
 package br.com.fiap.springpfentregas.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "TB_ENDERECO")
 public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_ENDERECO")
+    @SequenceGenerator(name = "SQ_ENDERECO", sequenceName = "SQ_ENDERECO", allocationSize = 1)
+    @Column(name = "ID_ENDERECO")
+    private Long id;
+
+    @Column(name = "CEP_ENDERECO")
+    private String cep;
+
+    @Column(name = "NUM_ENDERECO")
+    private String numero;
+
+    @Column(name = "COMP_ENDERECO")
+    private String complemento;
 }
