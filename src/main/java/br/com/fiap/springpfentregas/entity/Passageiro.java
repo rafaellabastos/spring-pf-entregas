116b98f5178e00ee872f6fed48ab9c7bf8cde1f3
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-03-18 16:27:43 -0300

    Classe passageiro implementada e mapeada

--- src/main/java/br/com/fiap/springpfentregas/entity/Passageiro.java
+++ src/main/java/br/com/fiap/springpfentregas/entity/Passageiro.java
@@ -1,4 +1,23 @@
 package br.com.fiap.springpfentregas.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "TB_PASSAGEIRO")
 public class Passageiro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PASSAGEIRO")
+    @SequenceGenerator(name = "SQ_PASSAGEIRO", sequenceName = "SQ_PASSAGEIRO", allocationSize = 1)
+    @Column(name = "ID_PASSAGEIRO")
+    private Long id;
 }
