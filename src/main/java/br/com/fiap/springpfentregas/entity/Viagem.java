e9f86d6e685438b46251f9689e9c1225153c6575
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-03-18 16:29:23 -0300

    Classe Viagem mapeada e implementada

--- src/main/java/br/com/fiap/springpfentregas/entity/Viagem.java
+++ src/main/java/br/com/fiap/springpfentregas/entity/Viagem.java
@@ -1,4 +1,23 @@
 package br.com.fiap.springpfentregas.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "TB_VIAGEM")
 public class Viagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_VIAGEM")
+    @SequenceGenerator(name = "SQ_VIAGEM", sequenceName = "SQ_VIAGEM", allocationSize = 1)
+    @Column(name = "ID_VIAGEM")
+    private Long id;
 }
