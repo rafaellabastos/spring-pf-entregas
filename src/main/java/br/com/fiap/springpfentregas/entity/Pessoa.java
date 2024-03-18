c1ec129d7f3a003ac200d66390f3bea703346301
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-03-18 16:32:20 -0300

    Classe Pessoa mapeada e implementad

--- src/main/java/br/com/fiap/springpfentregas/entity/Pessoa.java
+++ src/main/java/br/com/fiap/springpfentregas/entity/Pessoa.java
@@ -1,4 +1,28 @@
 package br.com.fiap.springpfentregas.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "TB_PESSOA")
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PESSOA")
+    @SequenceGenerator(name = "SQ_PESSOA", sequenceName = "SQ_PESSOA", allocationSize = 1)
+    @Column(name = "ID_PESSOA")
+    private Long id;
+
+    @Column(name = "NM_PESSOA")
+    private String nome;
+
+    private String email;
 }
