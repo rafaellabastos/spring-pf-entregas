3218817f946a001c6c95f67ed3ff911a5d49a383
LuizFFarias <rm99519@fiap.com.br>
2024-03-19 18:11:10 -0300

    Criando as classes PessoaResource e ViagemResource com o funcionamento do findAll, findById e save

---
+++ src/main/java/br/com/fiap/springpfentregas/resources/ViagemResource.java
@@ -0,0 +1,33 @@
+package br.com.fiap.springpfentregas.resources;
+
+import br.com.fiap.springpfentregas.entity.Viagem;
+import br.com.fiap.springpfentregas.repository.ViagemRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/viagem")
+public class ViagemResource {
+
+    @Autowired
+    private ViagemRepository viagemRepo;
+
+    @GetMapping
+    public List<Viagem> findAll(){
+        return viagemRepo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Viagem findById(@PathVariable Long id){
+        return viagemRepo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Viagem save(@RequestBody Viagem v){
+        return viagemRepo.save(v);
+    }
+}
